874987352ce127379b47487b3d6b4a351fd60f68
SamanthBommishetti <160122609+SamanthBommishetti@users.noreply.github.com>
2025-12-26 17:04:02 +0530

    Add files via upload
    bukcet trigger

---
+++ bucket.py
@@ -0,0 +1,16 @@
+import functions_framework
+
+@functions_framework.cloud_event
+def on_file_upload(cloud_event):
+    data = cloud_event.data or {}
+
+    bucket = data.get("bucket")
+    name = data.get("name")
+    size = data.get("size")
+    content_type = data.get("contentType")
+
+    print("CLOUD STORAGE EVENT RECEIVED")
+    print(f"Bucket: {bucket}")
+    print(f"File: {name}")
+    print(f"Size: {size} bytes")
+    print(f"Content Type: {content_type}")
